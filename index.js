f34f45e3b2a2382195dacea0a1834744e2c487cd
Ramesh <ramrajagopal@deloitte.com>
2018-11-15 14:18:57 +0530

    handling recipe req res

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const auth = {
-  username: "sunilhari",
-  password: "personal access token"
-};
 const express = require("express");
 const axios = require("axios");
 const PORT = process.env.PORT || 3001;
@@ -11,39 +7,42 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (request, response) => {
-  response.send("Star-Gazer Here ...");
+  response.send("Recipe Bot - Try a conversation to find your next meal");
 });
 app.post("/", (request, response) => {
   console.log(request.body.queryResult.parameters);
-  const { organization, repository } = request.body.queryResult.parameters;
-  axios(`https://api.github.com/repos/${organization}/${repository}`, {
-    auth
-  })
-    .then(res => {     
-      const {
-        description,
-        stargazers_count,
-        language,
-        html_url,
-        organization: { avatar_url }
-      } = res.data;
+  const { ingredients } = request.body.queryResult.parameters;
+  axios(`http://www.recipepuppy.com/api/?i=${ingredients}`)
+    .then(res => {
+      console.log(res);
+
+      const { results } = res.data;
       response.json({
-        fulfillmentText: `${repository} has ${stargazers_count} stars on github.To briefly descirbe about the project ${repository} is ${description}`,
-        fulfillmentMessages: [
-          {
+        fulfillmentText: `with ${ingredients} you can prepare below meals.`,
+        // fulfillmentMessages: [
+        //   {
+        //     card: {
+        //       title: `${repository} (${stargazers_count})`,
+        //       subtitle: `${description}`,
+        //       imageUri: avatar_url,
+        //       buttons: [
+        //         {
+        //           text: `${repository}`,
+        //           postback: `${html_url}`
+        //         }
+        //       ]
+        //     }
+        //   }
+        // ],
+        fulfillmentMessages: results.map(result => {
+          const { title, ingredients } = result;
+          return {
             card: {
-              title: `${repository} (${stargazers_count})`,
-              subtitle: `${description}`,
-              imageUri: avatar_url,
-              buttons: [
-                {
-                  text: `${repository}`,
-                  postback: `${html_url}`
-                }
-              ]
+              title,
+              ingredients
             }
-          }
-        ],
+          };
+        }),
         source: "github",
         payload: {
           google: {
@@ -52,7 +51,7 @@ app.post("/", (request, response) => {
               items: [
                 {
                   simpleResponse: {
-                    textToSpeech: `${repository} has ${stargazers_count} stars on github.To briefly descirbe about the project ${repository} is ${description}`
+                    textToSpeech: `with ${ingredients} you can prepare below meals.`
                   }
                 }
               ]
@@ -61,8 +60,8 @@ app.post("/", (request, response) => {
         }
       });
     })
-    .catch(() => {
-      console.log("Error")
+    .catch(err => {
+      console.log("Error", err);
     });
 });
 app.listen(PORT, () => {
